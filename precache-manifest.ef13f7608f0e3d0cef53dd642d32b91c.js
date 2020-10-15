b151b3a1d0b5208ca6649e656819a91912b1f28a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-15 16:01:46 +0000

    Deploy esogelola/3104-lab6 to github.com/esogelola/3104-lab6.git:gh-pages

---
+++ precache-manifest.ef13f7608f0e3d0cef53dd642d32b91c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d6878ccc25d92fad33e",
+    "url": "/static/css/main.f1933ed3.chunk.css"
+  },
+  {
+    "revision": "0d6878ccc25d92fad33e",
+    "url": "/static/js/main.0d6878cc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "0e04a58908b8f1fd14f92e5486fa2219",
+    "url": "/index.html"
+  }
+];
