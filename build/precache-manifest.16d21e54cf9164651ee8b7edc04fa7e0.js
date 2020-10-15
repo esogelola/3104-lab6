68c76713d8e868fefb6a45c76be710e678d16ad4
Emmanuel Sogelola <esogelola@gmail.com>
2020-10-15 01:21:50 -0400

    Updated package.json and added public

---
+++ build/precache-manifest.16d21e54cf9164651ee8b7edc04fa7e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "de01b2aedeb0f6309f61",
+    "url": "/esogelola/comp3104/static/css/main.8c15a66a.chunk.css"
+  },
+  {
+    "revision": "de01b2aedeb0f6309f61",
+    "url": "/esogelola/comp3104/static/js/main.de01b2ae.chunk.js"
+  },
+  {
+    "revision": "8a5d0a9a40288b4a84e6",
+    "url": "/esogelola/comp3104/static/js/runtime~main.8a5d0a9a.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/esogelola/comp3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "e514ebd221b3271b86407e656cabec9d",
+    "url": "/esogelola/comp3104/index.html"
+  }
+];
